61a6d927057b0a4fc899741782e8722f284adc25
xiaqingsong <xqslove@yeah.net>
2016-06-07 10:21:17 +0800

    first commit

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,10 +1,22 @@
+/*
+* 用户有关的处理
+* */
 var users = {
+    /*
+    * 首页
+    * */
     index: function (req, res, next) {
         res.render('index');
     },
+    /*
+    * 登录
+    * */
     login: function (req,res,next) {
         res.render('users/login', {title: 'login'});
     },
+    /*
+    * 用户中心
+    * */
     center: function (req, res, next) {
         res.render('users/center',{info: 'xqs'});
     }
