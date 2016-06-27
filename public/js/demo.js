3537615613e99b5a3d9f0142ced5b424e7fd1dff
xiaqingsong <xqslove@yeah.net>
2016-06-27 13:49:47 +0800

    update files

--- public/js/demo.js
+++ public/js/demo.js
@@ -45,7 +45,81 @@ var page = {
             }
         };
 
+        var navCenter = {
+            url: '/navCenter',
+            className: 'm-navCenter',
+            render: function () {
+                return $('#tpl_navCenter').html();
+            },
+            bind: function () {
+
+            }
+        };
+
+        /*下拉刷新*/
+        var dropload = {
+            url: '/dropload',
+            className: 'm-dropload',
+            render: function () {
+                return $('#tpl_dropload').html();
+            },
+            bind: function () {
+                // dropload 测试
+                M.dropLoad({
+                    callback: function (me) {
+                        setTimeout(function() {
+                            console.log('end');
+                            me.pullToRefreshDone();
+                        }, 1000);
+                    }
+                });
+
+                /*上拉加载分页*/
+                $.pagination({
+                    scrollBox: '.dropload',
+                    api: '/beans',
+                    container: '.dropload',
+                    fnSuccess: function (res, ele) {
+                        console.log(JSON.stringify(res))
+                    },
+                    fnFailed: function (res,ele) {
+                        ele.data('locked',false);
+                    }
+                });
+            }
+        };
+        /*鼠标滚动header展开或收缩*/
+        var headroom = {
+            url: '/headroom',
+            className: 'm-headroom',
+            render: function () {
+                return $('#tpl_headroom').html();
+            },
+            bind: function () {
+                //鼠标滚动上滑展开下滑收缩
+                $("#header").headroom({
+                    scroller: $("#spa")[0],
+                    classes: {
+                        initial: "animated",
+                        pinned: "fadeInDown", //向下拖动
+                        unpinned: "fadeOutUp"  //向上拖动
+                    }
+                });
+                $(".footer-nav .affix-bottom").headroom({
+                    scroller: $("#spa")[0],
+                    classes: {
+                        initial: "animated",
+                        pinned: "fadeInUp",
+                        unpinned: "fadeOutDown"
+                    }
+                });
+            }
+        };
+
         router.push(home)
+            .push(navCenter)
+            .push(headroom)
+            .push(dropload)
             .setDefault('/').init();
     }
 };
